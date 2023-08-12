1a4caef6a71303ed0556478481b760177b7d852d
NikSevenEleven <kursdz@mail.ru>
2023-08-13 00:45:58 +0300

    w.filters

--- resources/views/worker/index.blade.php
+++ resources/views/worker/index.blade.php
@@ -10,6 +10,24 @@
 <body>
 Index page
 <div>
+    <hr>
+    <div>
+        <form action="{{ route('worker.index') }}">
+            <input type="text" name="name" placeholder="name" value="{{ request()->get('name') }}">
+            <input type="text" name="surname" placeholder="surname" value="{{ request()->get('surname') }}">
+            <input type="text" name="email" placeholder="email" value="{{ request()->get('email') }}">
+            <input type="number" name="from" placeholder="from" value="{{ request()->get('from') }}">
+            <input type="number" name="to" placeholder="to" value="{{ request()->get('to') }}">
+            <input type="text" name="description" placeholder="description"
+                   value="{{ request()->get('description') }}">
+            <input id="isMarried" type="checkbox" name="is_married"
+                {{ request()->get('is_married') == 'on' ? ' checked' : ''}}
+            >
+            <label for="isMarried">Is married</label>
+            <input type="submit">
+            <a href="{{ route('worker.index') }}">Reset</a>
+        </form>
+    </div>
          <hr>
         @foreach($workers as $worker)
             <div>
@@ -39,7 +57,7 @@ Index page
         <hr>
         @endforeach
     <div class="my-nav">
-        {{$workers->links()}}
+        {{$workers->withQueryString()->links()}}
     </div>
 
 
